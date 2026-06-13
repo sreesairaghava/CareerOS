9c5302e4002ffb6500664c1d912115c1654c4d8c
Sree Sai Raghava <raghava.rym@gmail.com>
2026-06-13 17:10:56 +0530

    Initial Commit

---
+++ CareerOS/CareerOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  CareerOSApp.swift
+//  CareerOS
+//
+//  Created by Sree Sai Raghava Dandu on 13/06/26.
+//
+
+import SwiftUI
+
+@main
+struct CareerOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CareerOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CareerOS
+//
+//  Created by Sree Sai Raghava Dandu on 13/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
